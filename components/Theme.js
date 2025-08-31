9cd03141f29940dc0361cda3eee22c990b7ef2cc
coder-nitin07 <nitinthakur87504@gmail.com>
2025-08-31 22:51:03 +0530

    Theme Add

---
+++ components/Theme.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Theme = () => {
+  return (
+    <>
+        <p>Tokyo Night Muted</p>
+        <p>Tokyo Dark</p>
+        <p>Cosmic Nights</p>
+        <p>Late Night Coding Theme</p>
+        <p>Tokyo Night Blackout</p>
+        <p>Tokyo Righter</p>
+        <p>Chandrian Owl</p>
+        <p>Night Owl</p>
+    </>
+  )
+}
+
+export default Theme
